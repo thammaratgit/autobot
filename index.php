4103ca33e03cfae300d0901d654aac0b500d192b
thammarat <thammarat.contact@gmail.com>
2021-08-10 21:00:52 +0700

    Add recieve text from LINE API code

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php
-  echo "Hello AUTOBOT git, we success";
-  http_response_code(200);
-?>
+require_once('./vendor/autoload.php');
+// Namespace
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+$channel_token = 'QtISQCHKaGI3+yOaesnO0JBJB/ESKTXd8RPgrUs7RAYUU7X6ZuDxlDGHyauATc+4QDgBv4mpX6baq3ex9l1Oz15Uao6bgM8IIfFZBDY8YX+TN9dZSNBjqNqLonQWPuUtRahDUPEB8zK1ftH6D4nGWgdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'd66b6fd9ced99ca46a3b3fa2a15ab002';
+
+// Get message from Line API
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
+
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+
+      // Line API send a lot of event type, we interested in message only.
+      if ($event['type'] == 'message') {
+          switch($event['message']['type']) {
+              case 'text':
+                   // Get replyToken
+                   $replyToken = $event['replyToken'];
+
+                   // Reply message
+                   $respMessage = 'Hello, your message is '. $event['message']['text'];
+                   $httpClient = new CurlHTTPClient($channel_token);
+                   $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+                   $textMessageBuilder = new TextMessageBuilder($respMessage);
+                   $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+                  break;
+              }
+          }
+      }
+}
+echo "OK"
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '49cbb53ec63625904ae06d08ce42a037284b3904',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '49cbb53ec63625904ae06d08ce42a037284b3904',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '7.1.0',
+            'version' => '7.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => 'f81d282ccc5d09c7df8232d5465bb29559976933',
+            'dev_requirement' => false,
+        ),
+    ),
+);
